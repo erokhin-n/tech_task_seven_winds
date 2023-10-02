f762f68183ca48f84a3a59b41cea66e0a42af88d
nikita <erohinnikita100@gmail.com>
2023-10-02 19:00:41 +0530

    decomposition

--- src/table/SavedRows.tsx
+++ src/table/SavedRows.tsx
@@ -1,3 +1,167 @@
+import { TableBody, TableCell, TableRow, TextField } from "@mui/material"
+import { useDeleteRowMutation, useListRowsQuery, useUpdateRowMutation } from "../store/apiSlice"
+import { IRow } from "../interface"
+import { useState } from "react"
+import DeleteIcon from '@mui/icons-material/Delete';
+
 export default function SavedRows() {
+
+    const {data, isLoading: loadList} = useListRowsQuery()
+    const [deleteRow] = useDeleteRowMutation()
+    const [updateTableRow, {isLoading: updateLoad}] = useUpdateRowMutation()
+
+    const [editingId, setEditingId] = useState<number>(0)
+    const [edRowName, setEdRowName] = useState<string>('')
+    const [edSalary, setEdSalary] = useState<number>(0)
+    const [edEquipmentCosts, setEdEquipmentCosts] = useState<number>(0)
+    const [edMainCosts, setEdMainCosts] = useState<number>(0)
+    const [EdEstimatedProfit, setEdEstimatedProfit] = useState<number>(0)
+
+    function handleDoubleClick(id:number) {
+        setEditingId(id);
+        data!.map((row:IRow) => { 
+            if(id === row.id){ 
+                setEdRowName(row.rowName)
+                setEdSalary(row.salary)
+        }})
+    }
+
+    function changeEdTitle(value:string) {
+        setEdRowName(value)
+    }
+
+    function changeEdSalary(value:string) {
+        if(value.match(/^[0-9]+$/)){
+            const num = Number(value)
+            setEdSalary(num)
+        } else if(value === '') {
+            setEdSalary(0)
+        }   
+    }
+
+    function changeEdDevices(value:string){
+        if(value.match(/^[0-9]+$/)){
+            const num = Number(value)
+            setEdEquipmentCosts(num)
+        } else if(value === '') {
+            setEdEquipmentCosts(0)
+        }  
+    }
+
+    function changeEdExpense(value:string){
+        if(value.match(/^[0-9]+$/)){
+            const num = Number(value)
+            setEdMainCosts(num)
+        } else if(value === '') {
+            setEdMainCosts(0)
+        }    
+    }
+
+    function changeEdProfit(value:string) {
+        if(value.match(/^[0-9]+$/)){
+            const num = Number(value)
+            setEdEstimatedProfit(num)
+        } else if(value === '') {
+            setEdEstimatedProfit(0)
+        }
+    }
+
+    function changeTableRow() {
+        updateTableRow({
+            id: editingId,
+            equipmentCosts: edEquipmentCosts,
+            estimatedProfit: EdEstimatedProfit,
+            machineOperatorSalary: 0,
+            mainCosts: edMainCosts,
+            materials: 0,
+            mimExploitation: 0,
+            overheads: 0,
+            parentId: null,
+            rowName: edRowName,
+            salary: edSalary,
+            supportCosts: 0
+        })
+    }
+
+    function removeRow(id:number) {
+        deleteRow(id)
+    }
     
+    return (
+        <div>
+        {(data) && data.map((row:IRow)  => 
+            <TableRow key={row.id} onDoubleClick={() => handleDoubleClick(row.id)}>
+                <TableCell>
+                    <DeleteIcon onClick ={()=> removeRow(row.id)} />
+                </TableCell>
+                <TableCell>
+                    {editingId === row.id ? (
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            value={edRowName}
+                            onChange={(e) => changeEdTitle(e.target.value)}
+                            onKeyDown={changeTableRow}
+                        />
+                    ) : (
+                        row.rowName
+                    )}
+                </TableCell>
+                <TableCell>
+                    {editingId === row.id ? (
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            value={edSalary}
+                            onChange={(e) => changeEdSalary(e.target.value)}
+                        />
+                    ) : (
+                        row.salary
+                    )}
+                </TableCell>
+                <TableCell>
+                    {editingId === row.id ? (
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            value={edEquipmentCosts}
+                            onChange={(e) => changeEdDevices(e.target.value)}
+                        />
+                    ) : (
+                        row.equipmentCosts
+                    )}
+                </TableCell>
+                <TableCell>
+                    {editingId === row.id ? (
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            value={edMainCosts}
+                            onChange={(e) => changeEdExpense(e.target.value)}
+                        />
+                    ) : (
+                        row.mainCosts
+                    )}
+                </TableCell>
+                <TableCell>
+                    {editingId === row.id ? (
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            value={EdEstimatedProfit}
+                            onChange={(e) => changeEdProfit(e.target.value)}
+                        />
+                    ) : (
+                        row.estimatedProfit
+                    )}
+                </TableCell>
+            </TableRow>
+        )}
+        </div>
+    )
 }
--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -13,27 +13,17 @@ import { useCreateRowMutation, useDeleteRowMutation, useListRowsQuery, useUpdate
 import { SetStateAction, useState } from 'react';
 import { Row } from '../enums';
 import { IRow } from '../interface';
+import SavedRows from './SavedRows';
 
 export default function BasicTable() {
 
     const [createRow, {isLoading}] = useCreateRowMutation()
-    const {data, isLoading: loadList} = useListRowsQuery()
-    const [updateTableRow, {isLoading: updateLoad}] = useUpdateRowMutation()
-    const [deleteRow] = useDeleteRowMutation()
 
     const [rowName, setRowName] = useState<string>('')
     const [salary, setSalary] = useState<number>(0)
     const [equipmentCosts, setEquipmentCosts] = useState<number>(0)
     const [mainCosts, setMainCosts] = useState<number>(0)
     const [estimatedProfit, setEstimatedProfit] = useState<number>(0)
-
-    const [editingId, setEditingId] = useState<number>(0);
-    const [edRowName, setEdRowName] = useState<string>('')
-    const [edSalary, setEdSalary] = useState<number>(0)
-    const [edEquipmentCosts, setEdEquipmentCosts] = useState<number>(0)
-    const [edMainCosts, setEdMainCosts] = useState<number>(0)
-    const [EdEstimatedProfit, setEdEstimatedProfit] = useState<number>(0)
-    
 
     function changeTitle(value:string) {
         setRowName(value)
@@ -74,55 +64,6 @@ export default function BasicTable() {
             setEstimatedProfit(0)
         }
     }
- 
-    function handleDoubleClick(id:number) {
-        setEditingId(id);
-        data!.map((row:IRow) => { 
-            if(id === row.id){ 
-                setEdRowName(row.rowName)
-                setEdSalary(row.salary)
-        }})
-    }
-
-    function changeEdTitle(value:string) {
-        setEdRowName(value)
-    }
-
-    function changeEdSalary(value:string) {
-        if(value.match(/^[0-9]+$/)){
-            const num = Number(value)
-            setEdSalary(num)
-        } else if(value === '') {
-            setEdSalary(0)
-        }   
-    }
-
-    function changeEdDevices(value:string){
-        if(value.match(/^[0-9]+$/)){
-            const num = Number(value)
-            setEdEquipmentCosts(num)
-        } else if(value === '') {
-            setEdEquipmentCosts(0)
-        }  
-    }
-
-    function changeEdExpense(value:string){
-        if(value.match(/^[0-9]+$/)){
-            const num = Number(value)
-            setEdMainCosts(num)
-        } else if(value === '') {
-            setEdMainCosts(0)
-        }    
-    }
-
-    function changeEdProfit(value:string) {
-        if(value.match(/^[0-9]+$/)){
-            const num = Number(value)
-            setEdEstimatedProfit(num)
-        } else if(value === '') {
-            setEdEstimatedProfit(0)
-        }
-    }
 
     function handleEnterKeyCreate(e: React.KeyboardEvent<HTMLInputElement>) {
         if (e.key === 'Enter') {
@@ -143,27 +84,6 @@ export default function BasicTable() {
         }
     }
 
-    function changeTableRow() {
-        updateTableRow({
-            id: editingId,
-            equipmentCosts: edEquipmentCosts,
-            estimatedProfit: EdEstimatedProfit,
-            machineOperatorSalary: 0,
-            mainCosts: edMainCosts,
-            materials: 0,
-            mimExploitation: 0,
-            overheads: 0,
-            parentId: null,
-            rowName: edRowName,
-            salary: edSalary,
-            supportCosts: 0
-        })
-    }
-
-    function removeRow(id:number) {
-        deleteRow(id)
-    }
-
     return (
         <TableContainer component={Paper}>
             <Table aria-label="simple table">
@@ -178,80 +98,7 @@ export default function BasicTable() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {(data) && data.map((row:IRow)  => 
-                        <TableRow key={row.id} onDoubleClick={() => handleDoubleClick(row.id)}>
-                            <TableCell>
-                                <DeleteIcon onClick ={()=> removeRow(row.id)} />
-                            </TableCell>
-                            <TableCell>
-                                {editingId === row.id ? (
-                                    <TextField
-                                        variant="outlined"
-                                        size="small"
-                                        fullWidth
-                                        value={edRowName}
-                                        onChange={(e) => changeEdTitle(e.target.value)}
-                                        onKeyDown={handleEnterKeyCreate}
-                                    />
-                                ) : (
-                                    row.rowName
-                                )}
-                            </TableCell>
-                            <TableCell>
-                                {editingId === row.id ? (
-                                    <TextField
-                                        variant="outlined"
-                                        size="small"
-                                        fullWidth
-                                        value={edSalary}
-                                        onChange={(e) => changeEdSalary(e.target.value)}
-                                    />
-                                ) : (
-                                    row.salary
-                                )}
-                            </TableCell>
-                            <TableCell>
-                                {editingId === row.id ? (
-                                    <TextField
-                                        variant="outlined"
-                                        size="small"
-                                        fullWidth
-                                        value={edEquipmentCosts}
-                                        onChange={(e) => changeEdDevices(e.target.value)}
-                                    />
-                                ) : (
-                                    row.equipmentCosts
-                                )}
-                            </TableCell>
-                            <TableCell>
-                                {editingId === row.id ? (
-                                    <TextField
-                                        variant="outlined"
-                                        size="small"
-                                        fullWidth
-                                        value={edMainCosts}
-                                        onChange={(e) => changeEdExpense(e.target.value)}
-                                    />
-                                ) : (
-                                    row.mainCosts
-                                )}
-                            </TableCell>
-                            <TableCell>
-                                {editingId === row.id ? (
-                                    <TextField
-                                        variant="outlined"
-                                        size="small"
-                                        fullWidth
-                                        value={EdEstimatedProfit}
-                                        onChange={(e) => changeEdProfit(e.target.value)}
-                                    />
-                                ) : (
-                                    row.estimatedProfit
-                                )}
-                            </TableCell>
-                        </TableRow>
-                    )}
-                        
+                    <SavedRows />
                     <TableRow>
                         <TableCell>
                             <DescriptionIcon htmlColor="#7986cb" 
